9b616f8d4053c490e803606e7357a420e05a768e
Godsix <haoyueshangren@gmail.com>
2022-09-20 15:29:28 +0800

    1.更改包导出；

--- pychariot32/__init__.py
+++ pychariot32/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from .chrapi import CHRAPI, CHR_API_VERSION
+from .chrapi import CHR_API_VERSION, CHRAPI, LocalCHRAPI
 __version__ = '.'.join([str(x) for x in CHR_API_VERSION])
